aefe78bd4c891f3b9891b35cbe4271b025e0cb06
SuperStrain <1143124497@qq.com>
2022-05-23 23:53:48 +0800

    5月23日使用echart完成了评分板块的设计

--- app.py
+++ app.py
@@ -32,7 +32,19 @@ def movie():
 
 @app.route('/score')
 def score():
-    return render_template('score.html')
+    score=[]    #评分
+    num=[]      #每个评分所统计出的电影数量
+    con = sqlite3.connect("movie.db")
+    cur = con.cursor()
+    sql = "select score,count(score) from movie250 group by score"
+    data = cur.execute(sql)
+    for item in data:
+        score.append(item[0])
+        num.append(item[1])
+    cur.close()
+    con.close()
+
+    return render_template('score.html',score=score,num=num)
 
 @app.route('/team')
 def team():
